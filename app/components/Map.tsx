5f69f228d73b27ee624264b32137f7e01e9622d9
Steven <steventokms@gmail.com>
2026-06-09 00:02:25 +0800

    feat: render future MRT lines (Jurong Region, Cross Island, RTS) as dotted

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -778,6 +778,7 @@ export default function Map({
               "line-color": "#020b14",
               "line-width": 6.2,
               "line-opacity": 0.72,
+              "line-dasharray": [2, 3],
             },
           });
 
@@ -793,6 +794,7 @@ export default function Map({
               "line-color": ["get", "color"],
               "line-width": 3.8,
               "line-opacity": 0.82,
+              "line-dasharray": [2, 3],
             },
           });
 
